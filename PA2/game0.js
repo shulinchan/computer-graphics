7bf04b5372c61f8741efd79b6137cc0193051e4f
Shu Lin Chan <shulin.chan@gmail.com>
2018-03-12 14:41:15 -0400

    Change avatar to monkey

--- PA2/game0.js
+++ PA2/game0.js
@@ -111,11 +111,9 @@ The user moves a cube around the board trying to knock balls into a cone
 
 		// create the avatar
 		avatarCam = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1000 );
-		avatar = createAvatar();
-		avatar.translateY(20);
+		createAvatar();
 		avatarCam.translateY(-4);
 		avatarCam.translateZ(3);
-		scene.add(avatar);
 		gameState.camera = avatarCam;
 
 		//add balls, everything scales on "totalBalls"
@@ -383,20 +381,30 @@ The user moves a cube around the board trying to knock balls into a cone
 	}
 
 	function createAvatar(){
-		//var geometry = new THREE.SphereGeometry( 4, 20, 20);
-		var geometry = new THREE.BoxGeometry( 5, 5, 6);
-		var material = new THREE.MeshLambertMaterial( { color: 0xffff00} );
-		var pmaterial = new Physijs.createMaterial(material,0.9,0.5);
-		//var mesh = new THREE.Mesh( geometry, material );
-		var mesh = new Physijs.BoxMesh( geometry, pmaterial );
-		mesh.setDamping(0.1,0.1);
-		mesh.castShadow = true;
-
-		avatarCam.position.set(0,4,0);
-		avatarCam.lookAt(0,4,10);
-		mesh.add(avatarCam);
-
-		return mesh;
+		var loader = new THREE.JSONLoader();
+		loader.load("../models/suzanne.json", 
+			function ( geometry, materials ) {
+				console.log("loading suzanne");
+				var material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
+				var pmaterial = new Physijs.createMaterial(material,0.9,0.5);
+				var suzanne = new Physijs.BoxMesh( geometry, pmaterial );
+				console.log(JSON.stringify(suzanne.scale));// = new THREE.Vector3(4.0,1.0,1.0);
+				suzanne.position.x = 1;
+				suzanne.position.y = 1;
+				suzanne.castShadow = true;
+				avatarCam.position.set(0,4,0);
+				avatarCam.lookAt(0,4,10);
+				suzanne.add(avatarCam);
+				scene.add(suzanne);
+				avatar = suzanne;
+			},
+			function(xhr){
+				console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+			},
+			function(err){
+				console.log("error in loading: "+err);
+			}
+		)
 	}
 
 
